ca09ea0be3d105039aaec367c9879def0ad072c1
Shahed <shahedbd123@gmail.com>
2021-12-16 18:19:26 +0600

    blade template number 2nd running

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,18 +2,52 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Support\Facades\File;
+use Spatie\YamlFrontMatter\YamlFrontMatter;
 
 class Post
+
 {
-    public static function find($slug)
-    {
-        if (!file_exists($path = resource_path("/posts/{$slug}.html"))) 
-        {
-          throw new ModelNotFoundException();
-        }
-        return cache()->remember("posts.{$slug}", 1200,  fn() => file_get_contents($path));
-    }
+
+  public $title;
+  public $excerpt;
+  public $date;
+  public $body;
+  public $slug;
+
+  public function __construct($title, $excerpt, $date, $body, $slug)
+  {
+    $this->title = $title;
+    $this->excerpt = $excerpt;
+    $this->date = $date;
+    $this->body = $body;
+    $this->slug = $slug;
+  }
+
+  public static function all()
+  {
+    return cache()->rememberForever('post.all', function () {
+      return collect(File::files(resource_path("posts")))->map(fn ($file) =>
+      YamlFrontMatter::parseFile($file))->map(
+        fn ($document) =>
+        new Post(
+          $document->title,
+          $document->excerpt,
+          $document->date,
+          $document->body(),
+          $document->slug
+        ))
+        ->sortByDesc('date');
+    });
+
+  
+  }
+
+  public static function find($slug)
+  {
+
+    return static::all()->firstWhere('slug', $slug);
+  }
+
 }
